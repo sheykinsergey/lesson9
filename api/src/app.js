f5831898ebdba1c1c51b26413c1bac2bdcc20161
Sheykin Sergey <sheykinsergey@gmail.com>
2021-12-21 01:30:55 +0200

    add user routes

--- api/src/app.js
+++ api/src/app.js
@@ -1,2 +1,18 @@
-// eslint-disable-next-line no-console
-console.log('hello');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const config = require('./services/config');
+const userRoutes = require('./routes/users');
+
+const app = express();
+const port = config.appPort;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/users', userRoutes);
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ api/src/routes/users.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const db = require('../services/db');
+
+router.get('/', async (req, res) => {
+
+    const limit = req.query.limit;
+    const list = await db.select().from('users').orderBy('id').limit(limit);
+
+    res.send(list);
+});
+
+router.get('/:id', async (req, res) => {
+
+    const userId = req.params.id
+    await db.select().from('users').where('id', userId);
+    
+    res.send('Single record Ok');
+});
+
+router.post('/', async (req, res) => {
+  
+    const addUser = req.body.name;
+    await db('users').insert({name: addUser})
+
+    res.send(`add name ${addUser}`);
+});
+
+router.put('/:id', async (req, res) => {
+
+    const editUserId = req.params.id;
+    const editName = req.query.name;
+
+    await db('users').where({id: editUserId}).update({name: editName})
+  
+
+    res.send(`Update id ${editUserId} to ${editName} Ok`);
+});
+
+router.delete('/:id', async (req, res) => {
+
+    const delUser = req.params.id;
+    await db('users').where({id: delUser}).del();
+
+    res.send('Delete Ok');
+});
+
+module.exports = router;
